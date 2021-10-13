c7e709f432e96170c5d52b576cc63addb3fbf348
xdh <1393949909@qq.com>
2021-10-13 15:12:56 +0800

    all

--- pages/goods-detail/goods-detail.vue
+++ pages/goods-detail/goods-detail.vue
@@ -15,39 +15,82 @@
 			</view>
 		</view>
 		<view class="line"></view>
-		
+
 		<view class="box2">
 			<view>货号: SD156489784156</view>
 			<view>库存: {{info.stockQuantity}}</view>
 		</view>
 		<view class="line"></view>
-		
+
 		<view class="box3">
 			<view class="title">详情介绍</view>
 			<view class="content">{{info.zhaiyao}}</view>
+		</view>
+		<view class="goods-nav">
+			<uni-goods-nav :fill="true" :options="options" :buttonGroup="buttonGroup" @click="onClick"
+				@buttonClick="buttonClick" />
 		</view>
 	</view>
 </template>
 
 <script>
+	import uniGoodsNav from '../../uni_modules/uni-goods-nav/components/uni-goods-nav/uni-goods-nav'
 	export default {
 		data() {
 			return {
 				id: 0,
 				swipers: [],
-				info: {}
+				info: {},
+				options: [{
+					icon: 'headphones',
+					text: '客服'
+				}, {
+					icon: 'shop',
+					text: '店铺',
+					info: 2,
+					infoBackgroundColor: '#007aff',
+					infoColor: "red"
+				}, {
+					icon: 'cart',
+					text: '购物车',
+					info: 6
+				}],
+				buttonGroup: [{
+						text: '加入购物车',
+						backgroundColor: '#ff0000',
+						color: '#fff'
+					},
+					{
+						text: '立即购买',
+						backgroundColor: '#ffa200',
+						color: '#fff'
+					}
+				]
 			}
 		},
+		components: {
+			uniGoodsNav
+		},
 		methods: {
+			onClick(e) {
+				uni.showToast({
+					title: `点击${e.content.text}`,
+					icon: 'none'
+				})
+			},
+			buttonClick(e) {
+				console.log(e)
+				this.options[2].info++
+			},
 			async getSwipers() {
 				const res = await this.$myRequest({
 					url: 'api/getDetailSwipers?pid=' + this.id
 				})
 				this.swipers = res.data.message
 			},
-			async getGoodsById(){
+			async getGoodsById() {
 				const res = await this.$myRequest({
-					url: 'api/getGoodsById?id='+this.id
+					url: 'api/getGoodsById?id=' + this.id
 				})
 				this.info = res.data.message
 			}
@@ -78,6 +121,7 @@
 				font-size: 34rpx;
 				color: $shop-color;
 				line-height: 80rpx;
+
 				text:nth-child(2) {
 					font-size: 30rpx;
 					color: #ccc;
@@ -86,34 +130,44 @@
 				}
 			}
 		}
+
 		.box2 {
 			padding: 0 10px;
 			font-size: 30rpx;
 			line-height: 60rpx;
 		}
+
 		.box3 {
+			padding-bottom: 50px;
 			.title {
 				font-size: 30rpx;
 				padding-left: 10px;
 				border-bottom: 1px solid #eee;
 				line-height: 60rpx;
 			}
+
 			.content {
 				font-size: 26rpx;
 				padding: 10px;
 				color: #333;
 			}
-			
+
 		}
+
 		.goods-name {
 			font-size: 32rpx;
 			line-height: 60rpx;
 		}
 	}
-	
+
 	.line {
 		height: 10rpx;
 		width: 750rpx;
 		background: #eee;
+	}
+	.goods-nav {
+		position: fixed;
+		bottom: 0;
+		width: 100%;
 	}
 </style>
